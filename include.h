f1351a9f0fc72a3e3cfcd7fe3bfb7407547c4485
KJM <kjm99d@naver.com>
2023-03-19 01:24:41 +0900

    ✅ 테스트 코드 추가

---
+++ include.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+#include "lib/WinSysCommon/CommonAPI.h"
+#include "lib/WinSysMemoryControl/MemCtrlAPI.h"
+//#include "lib/WinSysCommon/CommonAPI.h"
--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 ﻿#include <iostream>
 
+
+#include "include.h"
+
 int main(int argc, const char * argv)
 {
+	HMODULE hCommon = nullptr;
+	HMODULE hWinHTTP = nullptr;
+	HMODULE hMemCtrl = nullptr;
 
+#if _WIN64
+	hCommon = LoadLibraryA("WinSysCommonx64.dll");
+	hWinHTTP = LoadLibraryA("WinSysHTTPx64.dll");
+	hMemCtrl = LoadLibraryA("WinSysMemCtrlx64.dll");
+#else
+	hCommon = LoadLibraryA("WinSysCommon.dll");
+	hWinHTTP = LoadLibraryA("WinSysHTTP.dll");
+	hMemCtrl = LoadLibraryA("WinSysMemCtrl.dll");
+#endif
+
+	FileCtrl_API::fp_CopyFile mCopyFile = (FileCtrl_API::fp_CopyFile)GetProcAddress(hCommon, "CopyFile");
+	mCopyFile((char*)"D:\\asd.txt", (char*)"D:\\asd2.txt");
 
 	return 0;
 }
